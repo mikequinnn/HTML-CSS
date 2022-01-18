2e60b2a4aa09747048f6c5a1dac0ef963a5e31b8
mikequinnn <maiquynhanhhh1302@gmail.com>
2022-01-18 12:55:17 +0700

    upload

--- buoi 7/script.js
+++ buoi 7/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-    var hiddenBox = $(".tab-document");
-    $("#tab-1").on("click", function(event) {
-        hiddenBox.show();
+    function activeTab(obj) {
+        $('button').removeClass('active');
+        $(obj).addClass('active');
+        var id = "#tab-" + $(obj).text().toLowerCase()
+        $('.tab-document').hide();
+        $(id).show();
+    }
+    $('button').click(function() {
+        activeTab(this);
+        return false;
     });
+    activeTab($('.btn button:first-child'));
 });
